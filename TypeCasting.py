f67829d3a4516069c8c79acd34648b9ff8a2311e
Noor- A- Alam <109654291+nooraalam1@users.noreply.github.com>
2024-10-15 00:09:32 +0600

    Add files via upload

---
+++ TypeCasting.py
@@ -0,0 +1,9 @@
+x = 5.5
+a = '3'
+print(type(x)) #Output <class 'float'>
+print(type(a)) #Output <class 'str'>
+
+x = int(x)
+a = int(a)
+print(type(x)) #Output <class 'int'>
+print(type(a)) #Output <class 'int'>
