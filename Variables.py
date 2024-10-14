64bca4abfff1b54e28cc6de817f901dc5cdc5be4
Noor- A- Alam <109654291+nooraalam1@users.noreply.github.com>
2024-10-15 00:03:40 +0600

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,20 @@
+a = 5
+A = 10
+print(a,A) #Output: 5 10
+
+#String---
+x = "hello"
+y = 'hello'
+#'hello' is the same as "hello"
+print(x,y) #Output hello hello
+
+#Multiple Value---
+e,f,g = 5,6,7
+print(e,f,g) #Output 5 6 7
+
+#Type()---
+print(type(5)) #Output: <class 'int'>
+print(type("Hello")) #Output: <class 'str'>
+print(type(5.55)) #Output: <class 'float'>
+print(type(True)) #Output: <class 'bool'>
+
