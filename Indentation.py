c971fe395b75bb7a5dc49f37cc028acfd2ae9782
Noor- A- Alam <109654291+nooraalam1@users.noreply.github.com>
2024-10-15 14:31:26 +0600

    Update Indentation.py

--- Indentation.py
+++ Indentation.py
@@ -1,2 +1,7 @@
+'''
+Unlike many other programming languages, where flower braces {} or other mechanism is used to define scope or a block of code, Python does not have those. Instead, it uses indentation.
+
+'''
+
 if 5 > 2:
  print("Five is greater than two!") 
