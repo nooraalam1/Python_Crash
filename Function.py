b45a65ac43b5f400fb83b6a857005ec708aa9788
Noor- A- Alam <109654291+nooraalam1@users.noreply.github.com>
2024-10-15 14:21:11 +0600

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,27 @@
+def my_func():
+    print("Printing from the function")
+
+my_func()
+
+#----------------------
+
+def my_func(name):
+    print("My name is "+name)
+
+my_func("Noor") #passing parameter
+
+#---------------------
+
+def my_func(first,last):
+    print("My name is "+ first + " "+last)
+
+my_func("Noor","A Alam")
+
+#---------------------
+
+def my_func(x):
+    return (x*5)
+
+print(my_func(2))
+
+#----------------------
