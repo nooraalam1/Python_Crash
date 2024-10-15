66274545daa9824c1781ffb35d147e6686868294
Noor- A- Alam <109654291+nooraalam1@users.noreply.github.com>
2024-10-15 12:23:20 +0600

    Add files via upload

---
+++ For.py
@@ -0,0 +1,36 @@
+# Print 1 to 10
+for x in range(10):
+    print(x)
+
+#---------------------
+
+for x in range(10):  
+    if x==5:
+        continue
+    print(x)
+
+#---------------------
+
+for x in range(1,20,4):
+    print(x) 
+
+'''
+Output:
+1
+5
+9
+13
+17
+'''
+
+#---------------------
+
+a = [1,2,3,4,5]
+b = [6,7,8,9,10]
+
+for x in a:
+    for y in b:
+        print(x,y) #Guess the output
+
+#---------------------
+
