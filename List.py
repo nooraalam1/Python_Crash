936adecc8daa1ae9cead1ab6e179574390aee2c5
Noor- A- Alam <109654291+nooraalam1@users.noreply.github.com>
2024-10-15 15:45:12 +0600

    Update List.py

--- List.py
+++ List.py
@@ -1,7 +1,8 @@
 '''
 Lists are used to store multiple items in a single variable.
 
-Lists are one of 4 built-in data types in Python used to store collections of data, the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage.
+Lists are one of 4 built-in data types in Python used to store collections of data, 
+the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage.
 
 Lists are created using square brackets.
 
