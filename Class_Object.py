d635b2beef48451fb58f254edacc9a6706d3d374
Noor- A- Alam <109654291+nooraalam1@users.noreply.github.com>
2024-10-16 09:44:38 +0600

    Add files via upload

---
+++ Class_Object.py
@@ -0,0 +1,21 @@
+class person:
+    def __init__(self,name,age):
+                self.name = name
+                self.age = age
+p1 = person("Noor",23)
+print(p1.name)
+print(p1.age)
+
+p2 = person("Alam",21)
+print(p2.name)
+print(p2.age)
+
+#---Methods---
+'''
+delete 
+del p1.name
+del p1
+
+
+
+'''
